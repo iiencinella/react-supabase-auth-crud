67290eebab15eb5c1d79fc43616d81119f925fb6
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2023-05-06 00:01:01 -0300

    Inclusion de react-router-dom y rutas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 import Login from './pages/login';
+import Home from './pages/home';
+import NotFound from './pages/notFound';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
