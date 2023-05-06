96435c75d156fc7f8e728886367124b801b74ded
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2023-05-06 01:16:57 -0300

    Create task

---
+++ src/components/taskForm.js
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import { clientSupabase } from '../supabase/client';
+
+function TaskForm() {
+  const [taskName, setTasName] = useState("")
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    try {
+      const user = await clientSupabase.auth.getUser()
+      const result = await clientSupabase.from('Tasks').insert({
+        name: taskName,
+        userId: user.data.user.id,
+      })
+      console.log(result)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="taskName" placeholder="Write a task name" onChange={e => setTasName(e.target.value)} />
+      <button>Add</button>
+    </form>
+  )
+}
+
+export default TaskForm
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,8 @@ import { useEffect } from 'react';
 import { clientSupabase } from '../supabase/client';
 import { useNavigate } from 'react-router-dom';
 
+import TaskForm from '../components/taskForm';
+
 export default function Home() {
   const navigate = useNavigate()
 
@@ -13,6 +15,8 @@ export default function Home() {
     <>
       <h2>Home</h2>
       <button onClick={() => clientSupabase.auth.signOut()}>Logout</button>
+
+      <TaskForm />
     </>
   )
 }
